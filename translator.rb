80d1fd7ee507d2298e625329a32e281258960c82
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-09-26 19:11:30 -0300

    fix linters erros

--- translator.rb
+++ translator.rb
@@ -1,57 +1,57 @@
 MORSE_CODE_TABLE = {
-  ".-" => "A",
-  "-..." => "B",
-  "-.-." => "C",
-  "-.." => "D",
-  "." => "E",
-  "..-." => "F",
-  "--." => "G",
-  "...." => "H",
-  ".." => "I",
-  ".---" => "J",
-  "-.-" => "K",
-  ".-.." => "L",
-  "--" => "M",
-  "-." => "N",
-  "---" => "O",
-  ".--." => "P",
-  "--.-" => "Q",
-  ".-." => "R",
-  "..." => "S",
-  "-" => "T",
-  "..-" => "U",
-  "...-" => "V",
-  ".--" => "W",
-  "-..-" => "X",
-  "-.--" => "Y", 
-  "--.." => "Z",  
-  "-----" => "0",
-  ".----" => "1",
-  "..---" => "2",
-  "...--" => "3",
-  "....-" => "4",
-  "....." => "5",
-  "-...." => "6",
-  "--..." => "7",
-  "---.." => "8",
-  "----." => "9",
-}
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z',
+  '-----' => '0',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '9'
+}.freeze
 
-def input_morse_code (user_input)
-  m_code = user_input.split("   ")
+def input_morse_code(user_input)
+  m_code = user_input.split('   ')
   m_code.map! { |n| change_into_word(n) }
   puts m_code.join
 end
 
 def change_into_word(word)
   letter_arr = word.split
-  letter_arr.map! do  |n| n = MORSE_CODE_TABLE[n] end
-  return letter_arr.join + " "
+  letter_arr.map! { |n| MORSE_CODE_TABLE[n] }
+  "#{letter_arr.join} "
 end
- 
 
 
-puts "Please enter some morse code in the terminal: "
+
+puts 'Please enter some morse code in the terminal: '
 
 user_input = gets.chomp
 
